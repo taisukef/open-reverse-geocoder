2e5f367940ce215a9aab19a8fe6096f0b5c30c5a
Taisuke Fukuno <fukuno@jig.jp>
2021-05-21 14:37:55 +0900

    add sample

--- src/sample.js
+++ src/sample.js
@@ -1,4 +1,4 @@
-import { openReverseGeocoder as geocoder } from './main.ts';
+import { openReverseGeocoder as geocoder } from 'https://taisukef.github.io/open-reverse-geocoder/src/main.js';
 const res = await geocoder([139.7673068, 35.6809591]);
 console.log(res);
 
