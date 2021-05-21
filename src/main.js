3de9a0b342296ded06d31f38af1042c03ec30d3a
Taisuke Fukuno <fukuno@jig.jp>
2021-05-21 14:30:29 +0900

    es version

--- src/main.js
+++ src/main.js
@@ -1,45 +1,15 @@
 import { geoContains } from "https://taisukef.github.io/d3-geo/src/index.js"
 import { lngLatToGoogle } from "https://taisukef.github.io/global-mercator/index.js"
-//import Protobuf from 'pbf'
-//1import axios from 'axios'
-//import { setupCache } from 'axios-cache-adapter'
 import { Pbf } from "https://taisukef.github.io/pbf/Pbf.js";
 import { VectorTile } from "https://taisukef.github.io/vector-tile-js/VectorTile.js";
 
-export interface ReverseGeocodingResult {
-  code: string
-  prefecture: string
-  city: string
-}
-
-type LngLat = [number, number]
-
-export interface ReverseGeocodingOptions {
-  /** どのズームを使うか。デフォルトは 10 */
-  zoomBase: number
-
-  /** タイルが入ってるURLフォーマット。 */
-  tileUrl: string
-}
-
-const DEFAULT_OPTIONS: ReverseGeocodingOptions = {
+const DEFAULT_OPTIONS = {
   zoomBase: 10,
   tileUrl: `https://geolonia.github.io/open-reverse-geocoder/tiles/{z}/{x}/{y}.pbf`,
 }
 
-//const cache = setupCache({
-//  maxAge: 60 * 60 * 24 * 1000
-//})
-
-//const api = axios.create({
-//  adapter: cache.adapter
-//})
-
-export const openReverseGeocoder: (
-  input: LngLat,
-  options?: Partial<ReverseGeocodingOptions>,
-) => Promise<ReverseGeocodingResult> = async (lnglat, inputOptions = {}) => {
-  const options: ReverseGeocodingOptions = {
+export const openReverseGeocoder = async (lnglat, inputOptions = {}) => {
+  const options = {
     ...DEFAULT_OPTIONS,
     ...inputOptions,
   }
@@ -81,7 +51,7 @@ export const openReverseGeocoder: (
         }
 
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        const res = geoContains(geojson as any, lnglat)
+        const res = geoContains(geojson, lnglat)
         if (res) {
           geocodingResult.code =
             5 === String(feature.id).length
--- src/main.test.js
+++ src/main.test.js
@@ -1,44 +1,45 @@
-import { openReverseGeocoder as geocoder } from './main'
+import * as t from "https://deno.land/std/testing/asserts.ts";
+import { openReverseGeocoder as geocoder } from './main.js';
 
-test('東京駅 [139.7673068, 35.6809591]', async () => {
+Deno.test('東京駅 [139.7673068, 35.6809591]', async () => {
   const res = await geocoder([139.7673068, 35.6809591])
-  expect(res).toStrictEqual({
+  t.assertEquals(res, {
     code: '13101',
     prefecture: '東京都',
     city: '千代田区',
   })
 })
 
-test('大阪駅 [135.4983028, 34.7055051]', async () => {
+Deno.test('大阪駅 [135.4983028, 34.7055051]', async () => {
   const res = await geocoder([135.4983028, 34.7055051])
-  expect(res).toStrictEqual({
+  t.assertEquals(res, {
     code: '27127',
     prefecture: '大阪府',
     city: '大阪市北区',
   })
 })
 
-test('串本町 [135.781478, 33.472551]', async () => {
+Deno.test('串本町 [135.781478, 33.472551]', async () => {
   const res = await geocoder([135.781478, 33.472551])
-  expect(res).toStrictEqual({
+  t.assertEquals(res, {
     code: '30428',
     prefecture: '和歌山県',
     city: '東牟婁郡串本町',
   })
 })
 
-test('北海道羅臼町 [145.189681, 44.021866]', async () => {
+Deno.test('北海道羅臼町 [145.189681, 44.021866]', async () => {
   const res = await geocoder([145.189681, 44.021866])
-  expect(res).toStrictEqual({
+  t.assertEquals(res, {
     code: '01694',
     prefecture: '北海道',
     city: '目梨郡羅臼町',
   })
 })
 
-test('八丈町 [139.785231, 33.115122]', async () => {
+Deno.test('八丈町 [139.785231, 33.115122]', async () => {
   const res = await geocoder([139.785231, 33.115122])
-  expect(res).toStrictEqual({
+  t.assertEquals(res, {
     code: '13401',
     prefecture: '東京都',
     city: '八丈町',
